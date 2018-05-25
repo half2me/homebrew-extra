4bd476e659fcc65c5969071a5992882c7ae9a077
Tamási Benjamin <half2me@users.noreply.github.com>
2018-05-25 11:10:46 +0200

    Update openalpr.rb

--- Formula/openalpr.rb
+++ Formula/openalpr.rb
@@ -5,36 +5,16 @@ class Openalpr < Formula
   sha256 "1cfcaab6f06e9984186ee19633a949158c0e2aacf9264127e2f86bd97641d6b9"
   head "https://github.com/openalpr/openalpr.git", :branch => "master"
 
-  option "without-daemon", "Do not include the alpr daemon (alprd)"
-
   depends_on "cmake" => :build
   depends_on "libtiff"
   depends_on "tesseract"
   depends_on "opencv"
 
-  if build.with? "daemon"
-    depends_on "log4cplus"
-    depends_on "beanstalkd"
-  end
-
   def install
     mkdir "src/build" do
       args = std_cmake_args
-
-      # v2.2.0 require CMAKE_MACOSX_RPATH
       args << "-DCMAKE_MACOSX_RPATH=true"
-      args << "-DCMAKE_INSTALL_SYSCONFDIR=#{etc}"
-
-      if build.without? "daemon"
-        if build.head?
-          args << "-DWITH_DAEMON=NO"
-        else
-          args << "-DWITHOUT_DAEMON=YES"
-        end
-      end
-
-      args << "-DCMAKE_INSTALL_SYSCONFDIR:PATH=#{etc}"
-
+      args << "-DWITH_DAEMON=NO"
       system "cmake", "..", *args
       system "make", "install"
     end
